efac3edac233ae63e968adcda13056937bbd02b1
Aldelme <alexandre.delme@etu.uca.fr>
2026-03-31 13:34:39 +0200

    Update defi.js

--- TP2/defi.js
+++ TP2/defi.js
@@ -1,1 +1,8 @@
+const bouton = document.queryselector('.menu-btn');
+const nav = document.queryselector('nav');
 
+bouton.addEventListener('click', () => {
+  nav.classList.toggle('is-open');
+  const isOpen = nav.classList.contains('is-open');
+  bouton.setAttribute('aria-expanded', isOpen);
+});
